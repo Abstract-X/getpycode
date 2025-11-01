10b6d70bb76359dae94c17acd00ea5f692f8f6ea
Abstract-X <abstract-x-mail@protonmail.com>
2025-11-01 18:03:07 +0300

    Add `ImportBuilder`

--- getpycode/__init__.py
+++ getpycode/__init__.py
@@ -1,9 +1,11 @@
 from .code_generator import CodeGenerator
+from .import_builder import ImportBuilder
 from .types import Package, Module
 
 
 __all__ = [
     "CodeGenerator",
+    "ImportBuilder",
     "Package",
     "Module"
 ]
